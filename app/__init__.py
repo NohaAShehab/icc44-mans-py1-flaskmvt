07635c59b6cabe797c088ae92a83ab252fadd410
Noha <nohashehab.iti@gmail.com>
2023-10-22 17:28:56 +0300

    generate migration files

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 ## in the init file ? create function that starts the app
 from flask import Flask,render_template
 from app.config import  project_config as App_Config
+from flask_migrate import Migrate
+
+
 from app.models import db
 # from app.students.views import  sayhello
 from app.students import  student_blueprint
@@ -12,6 +15,7 @@ def create_app(config_name='dev'):
     app.config["SQLALCHEMY_DATABASE_URI"]= Current_App_Config.SQLALCHEMY_DATABASE_URI
     app.config.from_object(Current_App_Config)
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     @app.errorhandler(404)
     def page_not_found(error):
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
-from flask_sqlalchemy import  SQLAlchemy
+import datetime
 
+from flask_sqlalchemy import  SQLAlchemy
+# from flask_sqlalchemy import func
 db = SQLAlchemy()
+
+class Student(db.Model):
+    __tablename__ ='students'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    grade = db.Column(db.Integer, default=10)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_onupdate=db.func.now(), server_default=db.func.now())
