b53963508276e1a1fd5a85c8d560974a3d84ad73
Noha <nohashehab.iti@gmail.com>
2023-10-22 16:36:03 +0300

    add db configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 ## in the init file ? create function that starts the app
 from flask import Flask
+from app.config import  project_config as App_Config
+from app.models import db
 
-
-def create_app():
+def create_app(config_name='dev'):
     app = Flask(__name__)
-
+    Current_App_Config = App_Config[config_name]
+    app.config["SQLALCHEMY_DATABASE_URI"]= Current_App_Config.SQLALCHEMY_DATABASE_URI
+    app.config.from_object(Current_App_Config)
+    db.init_app(app)
     return app
--- app/config.py
+++ app/config.py
@@ -7,16 +7,16 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    DEBUG=1
+    DEBUG=True
     SQLALCHEMY_DATABASE_URI= "sqlite:///project.sqlite"
 
 
 class ProductionConfig(Config):
-    DEBUG = 0
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = "postgresql://flask:iti@localhost:5432/flask_py1"
 
 
-config = {
+project_config = {
     'dev': DevelopmentConfig,
     "prd": ProductionConfig
 }
